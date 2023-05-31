62b8e8175c6bd6166452954982eb3ea8cbe8a060
fidelisaboke <fidelisaboke@gmail.com>
2023-05-31 08:19:44 +0300

    Modified sign_in

---
+++ project_forms/sign_in.php
@@ -0,0 +1,34 @@
+<html>
+    <head>
+        <title> Sign in page </title>
+        <style>
+            body{
+                background: aliceblue;
+            }
+            form{
+                display: flex;
+                flex-direction: column;
+                border: 1px solid black;
+                border-radius: 20px;
+                background: white;
+                align-items: center;
+            }
+        </style>
+    </head>
+
+<h1>DRUG DISPENSARY NAME</h1>
+<h2>SIGN IN</h2>
+
+<form >
+    <input type="text" placeholder="username"><br>
+    <input type="password" placeholder="password"><br>
+    
+    <p>Show password</p>
+    <input type="radio" id="password">
+    <label for="password">forgot password</label><br>
+    <button>Clear</button>
+    <button>Confirm</button><br>
+    <p>New user?</p>
+    </form>
+    </html>
+
