2a4ccd9b5158256b08ebe534fbbdabe4e0f63231
fidelisaboke <fidelisaboke@gmail.com>
2023-06-14 09:32:47 +0300

    Linked the stylesheets to the webpages

--- project_forms/register.php
+++ project_forms/register.php
@@ -1,10 +1,9 @@
+<!DOCTYPE html>
 <html>
     <head>
+        <link rel="stylesheet" href="./styles/register.css">
         <title>Registration page</title>
     </head>
-    <style>
-        
-    </style>
     <body>
         <h>DRUG DISPENSARY NAME</h>
         <img alt="image of logo">
@@ -18,15 +17,14 @@
              <option>Pharmaceutical company director</option>
              <option>Pharmacist</option>
              <option>Supervisor</option>
-             </select>
-             <input type="password" placeholder="password">
-             <input type="text" placeholder="Confirm password">
-             <input type="radio" id="show">
-             <label for="show">Show password</label>
-             <button>Clear</button>
-             <button>Confirm</button>
-             <a href="Sign in page.html" target="_blank">Already have an account</a><br>
-            
+            </select>
+            <input type="password" placeholder="password">
+            <input type="text" placeholder="Confirm password">
+            <input type="radio" id="show">
+            <label for="show">Show password</label>
+            <button>Clear</button>
+            <button>Confirm</button>
+            <a href="Sign in page.html" target="_blank">Already have an account</a><br>
         </form>
     </body>
     </html>
--- project_forms/sign_in.php
+++ project_forms/sign_in.php
@@ -1,22 +1,24 @@
+<!DOCTYPE html>
 <html>
     <head>
+        <link rel="stylesheet" href="./styles/sign_in.css">
         <title> Sign in page </title>
-        
     </head>
+    <body>
+        <h1>DRUG DISPENSARY NAME</h1>
+        <h2>SIGN IN</h2>
 
-<h1>DRUG DISPENSARY NAME</h1>
-<h2>SIGN IN</h2>
-
-<form >
-    <input type="text" placeholder="username"><br>
-    <input type="password" placeholder="password"><br>
-    
-    <p>Show password</p>
-    <input type="radio" id="password">
-    <label for="password">forgot password</label><br>
-    <button>Clear</button>
-    <button>Confirm</button><br>
-    <p>New user?</p>
-    </form>
-    </html>
+        <form>
+            <input type="text" placeholder="username"><br>
+            <input type="password" placeholder="password"><br>
+            
+            <p>Show password</p>
+            <input type="radio" id="password">
+            <label for="password">forgot password</label><br>
+            <button>Clear</button>
+            <button>Confirm</button><br>
+            <p>New user?</p>
+        </form>
+    </body>
+</html>
 
