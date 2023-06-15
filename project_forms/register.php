10a72fd5c6a2b98f33f93941e74fa5e8f0ba6654
fidelisaboke <fidelisaboke@gmail.com>
2023-06-15 10:05:14 +0300

    Made massive changes in designing user sign up

--- project_forms/register.php
+++ project_forms/register.php
@@ -7,24 +7,24 @@
     <body>
         <h>DRUG DISPENSARY NAME</h>
         <img alt="image of logo">
-        <form>
+        <form action="./process/process_register_details.php" method="POST">
             <h3>REGISTER</h3>
-            <input type="text" placeholder="username">
+            <input type="text" placeholder="username"><br>
             <label for="register"></label>
-            <select id="register">
-             <option> Patient</option>
-             <option>Doctor</option>
-             <option>Pharmaceutical company director</option>
-             <option>Pharmacist</option>
-             <option>Supervisor</option>
+            <select name="user-type" id="register">
+                <option value="patient">Patient</option>
+                <option value="doctor">Doctor</option>
+                <option value="pharmaceutical_company">Pharmaceutical Company</option>
+                <option value="pharmacist">Pharmacist</option>
+                <option value="supervisor">Supervisor</option>
             </select>
-            <input type="password" placeholder="password">
-            <input type="text" placeholder="Confirm password">
-            <input type="radio" id="show">
+            <input type="password" placeholder="password"><br>
+            <input type="text" placeholder="Confirm password"><br>
+            <input type="checkbox" id="show">
             <label for="show">Show password</label>
             <button>Clear</button>
-            <button>Confirm</button>
-            <a href="Sign in page.html" target="_blank">Already have an account</a><br>
+            <input type="submit" value="Submit"><br>
+            <a href="Sign in page.html" target="_blank">Already have an account?</a>
         </form>
     </body>
     </html>
