f775177a31fb494bc2b68a7905b876a769582835
fidelisaboke <fidelisaboke@gmail.com>
2023-06-16 12:05:04 +0300

    Modified the patient_menu to display the username on the top right

--- project_forms/user_menu/patient_menu.php
+++ project_forms/user_menu/patient_menu.php
@@ -4,8 +4,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Patient Menu</title>
+    <link rel="stylesheet" href="../styles/patient_menu.css">
 </head>
 <body>
-    <h1>Patient Menu</h1>
+    <?php
+        // Resume the session started by the erifyUserDetails() function and reclaim the user_name:
+        session_start();
+        $user_name = $_SESSION['user_name'];
+    ?>
+    <header class="page-header">
+         <!--Displaying the username on the page-->
+        <span id="welcome-username">Welcome, <?php echo $user_name; ?></span>
+        <h1>Patient Menu</h1>
+       
+    </header>
 </body>
 </html>
