025ec09e7c4e217f4eac9912254930752d1d64af
fidelisaboke <fidelisaboke@gmail.com>
2023-06-21 10:15:28 +0300

    Modified table styles, added placeholder in sign-in page

--- src/sign_in.php
+++ src/sign_in.php
@@ -12,7 +12,7 @@
             </div>
             <div class="form-content">
                 <h2>SIGN IN</h2>
-                <input type="text" placeholder="Username" id="user_name" name="user_name"><br>
+                <input type="text" placeholder="Email or Username" id="user_name" name="user_name"><br>
                 <input type="password" placeholder="Enter password..." id="user_pass" name="user_pass"><br>
                 <div class="form-buttons">
                     <button type="submit" name="submit" value="submit">Submit</button><br>
