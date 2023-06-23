f2519c6bc71c6396f31736354625761085dc4b8b
fidelisaboke <fidelisaboke@gmail.com>
2023-06-23 22:01:55 +0300

    Added pagination

--- src/classes/views/tableview.class.php
+++ src/classes/views/tableview.class.php
@@ -1,20 +1,28 @@
 <?php
 class TableView{
-    public static function showTable($table_data, $item_name){
-        if(count($table_data)>0){
+    public static function showTable($table_data, $item_name, $itemsPerPage = 10) {
+        // Get the current page number from the URL, default to 1 if not set
+        $currentPage = isset($_GET['page']) ? $_GET['page'] : 1;
+
+        // Calculate the start and end index of the data to display on the current page
+        $startIndex = ($currentPage - 1) * $itemsPerPage;
+        $endIndex = min($startIndex + $itemsPerPage, count($table_data));
+
+        if (count($table_data) > 0) {
             echo "<table>";
             $keys = array_keys($table_data[0]); // get the columns of the table from the first array
-            $id=$keys[0];
+            $id = $keys[0];
             echo "<tr>";
-            foreach($keys as $key){
-                echo "<th scope='col'>".$key."</th>"; // display the attributes as headers for the table
+            foreach ($keys as $key) {
+                echo "<th scope='col'>" . $key . "</th>"; // display the attributes as headers for the table
             }
             echo "<th colspan=2>Action</th>";
             echo "</tr>";
-            foreach($table_data as $row){ // loop through each row of the table (the 2d array)
+            for ($i = $startIndex; $i < $endIndex; $i++) {
+                $row = $table_data[$i];
                 echo "<tr>";
-                foreach($row as $value){
-                    echo "<td>".$value."</td>"; // display the records (values) in the HTML table
+                foreach ($row as $value) {
+                    echo "<td>" . $value . "</td>"; // display the records (values) in the HTML table
                 }
                 $identifier = $row[$id];
                 echo "<td><a class='btn-links btn-update' href='../update/update_$item_name.php?id=$identifier'>Update</a></td>";
@@ -22,8 +30,17 @@ class TableView{
                 echo "</tr>";
             }
             echo "</table>";
+
+            // Display the pagination! (they are just hyperlinks for now)
+            $pageNumbers = PageView::Pagination(count($table_data), $itemsPerPage, $currentPage);
+            echo '<div class="pagination">';
+            foreach ($pageNumbers as $pageNumber) {
+                echo '<a href="?page=' . $pageNumber . '">' . $pageNumber . '</a> ';
+            }
+            echo '</div>';
         }
         echo "<br>";
         echo "<a class='btn-links btn-new' href='../add/add_$item_name.php'>Add New Record</a>";
     }
-}?>
+}
+?>
