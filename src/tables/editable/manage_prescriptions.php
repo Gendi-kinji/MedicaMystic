bf6496fa51ed79e21a786742894497ddc5461f49
Gendi-kinji <kithinjigeorge192@gmail.com>
2023-07-19 21:03:36 +0300

    Added code for view table

--- src/tables/editable/manage_prescriptions.php
+++ src/tables/editable/manage_prescriptions.php
@@ -1,9 +1,9 @@
 <?php
-    require "../..classes/connection.class.php";
-    require "../..classes/databasehandler.class.php";
-    require "../..classes/models/prescriptions.class.php";
-    require "../..classes/views/pageview.class.php";
-    require "../..classes/views/tableview.class.php";
+    require "../../connection.class.php";
+    require "../../databasehandler.class.php";
+    require "../../models/prescriptions.class.php";
+    require "../../views/pageview.class.php";
+    require "../../views/tableview.class.php";
 ?>
 
 <!DOCTYPE html>
@@ -14,12 +14,11 @@
         <title>Pharmacy Details</title>
         <link rel="stylesheet" href="../../styles/table_styles.css">
     </head>
-    <body>
-        <h1>Pharmacy Table</h1>
+    <body></body><h1>Prescription Table</h1>
             <?php
-            $pharmacy = new Pharmacy();
-            $pharmacy_table = $pharmacy->getAllPharmacies();
-            TableView::showEditableTable($pharmacy_table, 'pharmacy');
+            $prescription = new Prescription();
+            $prescription_table = $prescription->getAllPrescriptions();
+            TableView::showEditableTable($prescription_table, 'prescription');
             ?>
     </body>
 </html>
