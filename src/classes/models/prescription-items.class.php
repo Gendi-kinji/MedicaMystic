43f91e9096206d0ad2661855343812f86416f701
fidelisaboke <fidelisaboke@gmail.com>
2023-07-19 15:17:23 +0300

    Added prescription classes, modified invoice items

---
+++ src/classes/models/prescription-items.class.php
@@ -0,0 +1,26 @@
+<?php
+class PrescriptionItem extends DatabaseHandler{
+    public function __construct(){
+        
+    }
+    public function addPrescriptionItem($itemsData) {
+        $this->setData('tbl_prescription_items', $itemsData);
+    }
+    public function getPrescriptionItem($search_value){
+        return $this->getData('tbl_prescription_items', 'presc_item_id', $search_value);
+    }
+
+    public function getAllPrescriptionItems(){
+        return $this->getTable('tbl_prescription_items');
+    }
+    public function getIDs(){
+        return $this->getColumn('presc_item_id', 'tbl_prescription_items');
+    }
+    public function updatePrescriptionItem($items_data, $unique_value){
+        $this->updateData('tbl_prescription_items', 'presc_item_id', $items_data, $unique_value);
+    }
+    public function deleteprescriptionItem($unique_value){
+        $this->deleteData('tbl_prescription_items','presc_item_id', $unique_value);
+    }
+}
+?>
---
+++ src/classes/models/prescription.class.php
@@ -0,0 +1,27 @@
+<?php
+class Prescription extends DatabaseHandler{
+    public function __construct(){
+        
+    }
+    public function addPrescription($prescriptionData, $return_insert_id = true) {
+        return $this->setData('tbl_prescriptions', $prescriptionData, $return_insert_id);
+    }
+    public function getPrescription($search_value){
+        return $this->getData('tbl_prescriptions', 'prescription_id', $search_value);
+    }
+
+    public function getAllPrescriptions(){
+        return $this->getTable('tbl_prescriptions');
+    }
+    public function getIDs(){
+        return $this->getColumn('prescription_id', 'tbl_prescriptions');
+    }
+    public function updatePrescription($Prescription_data, $unique_value){
+        $this->updateData('tbl_prescriptions', 'prescription_id', $Prescription_data, $unique_value);
+    }
+    public function deletePrescription($unique_value){
+        $this->deleteData('tbl_prescriptions','prescription_id', $unique_value);
+
+    }
+}
+?>
