8655672f5d728648f4e325fb4b995e2f73432e70
Gendi kinji <kithinjigeorge192@gmail.com>
2023-09-16 19:18:54 +0300

    Added

---
+++ src/drug_dashboard.php
@@ -0,0 +1,27 @@
+<?php 
+include 'inc/autoloader.inc.php';
+include 'inc/classes/models/Drug.class.php';
+include  'common_sections/topbar.php';
+
+?>
+<head>
+
+
+<title><?php 
+$pageTitle="Drug Dashboard";
+echo $pageTitle; ?></title>
+    <nav>
+          <a href=""?id=$id></a>
+    </nav>
+</head>
+<body>
+    
+       <img src="$_POST[]">
+       <a href="">View Details</a>
+
+       
+
+
+    
+</body>
+</html>
--- src/inc/classes/models/Drug.class.php
+++ src/inc/classes/models/Drug.class.php
@@ -1,4 +1,5 @@
 <?php
+include 'inc/autoloader.inc.php';
 class Drug extends DatabaseHandler{
 
     public function __construct(){
