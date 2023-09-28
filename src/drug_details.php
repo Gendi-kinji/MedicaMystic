d54cdc8203e6c6c3e361ea8e30e5efe2b369589a
Gendi kinji <kithinjigeorge192@gmail.com>
2023-09-28 12:08:22 +0300

    Added HTML elemets to the drug_details page

--- src/drug_details.php
+++ src/drug_details.php
@@ -1,6 +1,20 @@
 <?php
 include 'inc/autoloader.inc.php';
-require_once 'drug_display.php';
+require_once 'drug_display.php';?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>
+        <?php
+        $pageTitle='Drug Details';
+        echo $pageTitle;
+        ?>
+    </title>
+</head>
+<body>
+    <?php
 if(isset($_GET['id'])){
     $id = $_GET['id'];
     generateImage($id);
@@ -23,3 +37,5 @@ foreach($drug_info as $drug=>$value){
 
 }
 ?>
+</body>
+</html>
